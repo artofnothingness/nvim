1f727ec260ba1d07a31f38c31e04fa065e590ebb
Alex <speedy_sky@mail.ru>
2021-04-10 15:13:19 +0300

    typo

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -2,7 +2,7 @@ require'lspconfig'.clangd.setup{
     cmd = { "clangd-11", "--background-index" },
     handlers = {["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { 
                 virtual_text = false,
-                signs = ture,
+                signs = true,
                 underline = true,
                 update_in_insert = true,
               })}
