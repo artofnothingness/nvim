17c07d6429a6545850ce114a560586fa38338ee4
Alex <speedy_sky@mail.ru>
2021-04-10 15:13:53 +0300

    typo

--- lua/lsp/python.lua
+++ lua/lsp/python.lua
@@ -1,7 +1,7 @@
 require'lspconfig'.pyright.setup{
     handlers = {["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { 
                 virtual_text = false,
-                signs = ture,
+                signs = true,
                 underline = true,
                 update_in_insert = true,
               })}
