0b645de4819e4f5266f245ed74e376ae3811bf42
Alex <speedy_sky@mail.ru>
2021-04-25 01:34:33 +0300

    change formatter

--- lua/code/lang/efm.lua
+++ lua/code/lang/efm.lua
@@ -1,4 +1,5 @@
-local python_arguments = {}
+local python_args = {}
+local lua_args = {}
 
 local flake8 = {
     LintCommand = "flake8 --ignore=E501 --stdin-display-name ${INPUT} -",
@@ -6,21 +7,24 @@ local flake8 = {
     lintFormats = {"%f:%l:%c: %m"}
 }
 
-local black = {formatCommand = "black --quiet -l 100 -", formatStdin = true}
+local autopep8 = {formatCommand = "autopep8 --max-line-length 100 -", formatStdin = true}
+local lua_fmt = {formatCommand = "lua-format -i", formatStdin = true}
 
--- table.insert(python_arguments, flake8)
-table.insert(python_arguments, black)
+-- table.insert(python_args, flake8)
+table.insert(python_args, autopep8)
+table.insert(lua_args, lua_fmt)
 
 require"lspconfig".efm.setup {
     handlers = lang.handlers,
     cmd = {"/home/alex/go/bin/efm-langserver"},
     init_options = {documentFormatting = true, codeAction = false},
 
-    filetypes = {"python"},
+    filetypes = {"python", "lua"},
     settings = {
         rootMarkers = {".git/"},
         languages = {
-            python = python_arguments,
+            python = python_args,
+            lua = lua_args,
         }
     }
 }
