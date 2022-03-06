a15587c00cb7997abd69439df1ff3ca6c8c2e308
Alex <aibudyakov@sberautotech.ru>
2022-03-06 16:57:40 +0300

    upda

--- utils/install-nvim.sh
+++ utils/install-nvim.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:neovim-ppa/unstable -y 
 sudo apt-get update 
-sudo apt-get install neovim -y
+sudo apt-get install neovim=0.7.0~ubuntu1+git202203060047-d557a4557-dd05b3569-fddb81c5a~ubuntu20.04.1 -y
 sudo apt-get install clangd-12 -y
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
