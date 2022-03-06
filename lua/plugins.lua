9ee1907ef684bb272ae98ce706600b7754f2e61c
alex <budyakov.aleksei@gmail.com>
2022-03-06 12:34:39 +0000

    update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -248,4 +248,16 @@ use {
   end
 }
 
+use {
+  "folke/trouble.nvim",
+  requires = "kyazdani42/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
+
 end)
