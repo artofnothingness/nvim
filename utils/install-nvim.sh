14977fe0c37c0fccfdb590cdeb541694f259670e
artofnothingness <speedy_sky@mail.ru>
2022-03-23 02:37:58 +0300

    update

--- utils/install-nvim.sh
+++ utils/install-nvim.sh
@@ -1,8 +1,13 @@
+git clone https://github.com/neovim/neovim.git
+cd neovim 
+
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd ..
+
+sudo rm -rf neovim
+
 sudo apt update
-sudo apt-get install software-properties-common -y
-sudo add-apt-repository ppa:neovim-ppa/unstable -y 
-sudo apt-get update 
-sudo apt-get install neovim=0.7.0~ubuntu1+git202203110047-6170574d2-dd05b3569-194686d0d~ubuntu20.04.1 -y
 sudo apt-get install clangd-12 ripgrep xclip -y
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 pip install cmake-language-server
