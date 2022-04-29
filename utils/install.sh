e31fd376ee61bcae44d05a0fa051d7e776da9606
artofnothingness <speedy_sky@mail.ru>
2022-04-30 01:34:52 +0300

    update

--- utils/install.sh
+++ utils/install.sh
@@ -2,5 +2,5 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install neovim -y
 
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
