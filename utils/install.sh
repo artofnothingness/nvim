e9cfe3ee52458da623771b9d2a876e2e3a411759
artofnothingness <speedy_sky@mail.ru>
2022-05-03 12:29:11 +0300

    update

--- utils/install.sh
+++ utils/install.sh
@@ -2,5 +2,10 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install neovim -y
 
+sudo apt install clangd-12 ripgrep xclip -y
+sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
+pip install cmake-language-server
+pip install pynvim
+
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
