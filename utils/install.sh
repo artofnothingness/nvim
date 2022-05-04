d0a2399b91d71f23c324fadfa3094b83ddcebcd9
artofnothingness <speedy_sky@mail.ru>
2022-05-04 20:11:58 +0300

    update

--- utils/install.sh
+++ utils/install.sh
@@ -7,5 +7,5 @@ sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 pip install cmake-language-server
 pip install pynvim
 
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' > /dev/null 2>&1
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
