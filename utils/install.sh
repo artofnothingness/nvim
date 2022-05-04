fbb0fdd3e8a4bedfb16bf9952438bc28b173d3cb
Alex <aibudyakov@sberautotech.ru>
2022-05-04 17:58:00 +0300

    upd

--- utils/install.sh
+++ utils/install.sh
@@ -2,7 +2,7 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install neovim -y
 
-sudo apt python3-pip install clangd-12 ripgrep xclip -y
+sudo apt install python3-pip clangd-12 ripgrep xclip -y
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
 pip install cmake-language-server
 pip install pynvim
