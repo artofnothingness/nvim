f6d5552478b3513b724db4f2ce8269b65f56b1bf
artofnothingness <speedy_sky@mail.ru>
2022-06-19 13:29:53 +0300

    update dashboard

--- lua/cfg/dashboard.lua
+++ lua/cfg/dashboard.lua
@@ -1,40 +1,48 @@
-vim.g.dashboard_custom_header = {
-             "=================     ===============     ===============   ========  ========",
-             "\\\\ . . . . . . .\\\\   //. . . . . . .\\\\   //. . . . . . .\\\\  \\\\. . .\\\\// . . //",
-             "||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\\/ . . .||",
-             "|| . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||",
-             "||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||",
-             "|| . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\\ . . . . ||",
-             "||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\\_ . .|. .||",
-             "|| . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\\ `-_/| . ||",
-             "||_-' ||  .|/    || ||    \\|.  || `-_|| ||_-' ||  .|/    || ||   | \\  / |-_.||",
-             "||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \\  / |  `||",
-             "||    `'         || ||         `'    || ||    `'         || ||   | \\  / |   ||",
-             "||            .===' `===.         .==='.`===.         .===' /==. |  \\/  |   ||",
-             "||         .=='   \\_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \\/  |   ||",
-             "||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \\/  |   ||",
-             "||   .=='    _-'          `-__\\._-'         `-_./__-'         `' |. /|  |   ||",
-             "||.=='    _-'                                                     `' |  /==.||",
-             "=='    _-'                        N E O V I M                         \\/   `==",
-             "\\   _-'                                                                `-_   /",
-             " `''                                                                      ``'  ",
-             "                                                                               ",
-             }
+local home = os.getenv('HOME')
 
-vim.g.dashboard_default_executive = 'telescope'
+local db = require('dashboard')
 
-vim.g.dashboard_custom_section = {
-    a = {description = {'  Recently Used Files'}, command = 'Telescope oldfiles'},
-    b = {description = {'  Find File          '}, command = 'Telescope find_files'},
-    c = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
-    d = {description = {'  New File           '}, command = 'enew'},
-    e = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
-    f = {description = {'  Settings           '}, command = ':e ~/.config/nvim/init.vim'},
+db.custom_header = {
+  "=================     ===============     ===============   ========  ========",
+  "\\\\ . . . . . . .\\\\   //. . . . . . .\\\\   //. . . . . . .\\\\  \\\\. . .\\\\// . . //",
+  "||. . ._____. . .|| ||. . ._____. . .|| ||. . ._____. . .|| || . . .\\/ . . .||",
+  "|| . .||   ||. . || || . .||   ||. . || || . .||   ||. . || ||. . . . . . . ||",
+  "||. . ||   || . .|| ||. . ||   || . .|| ||. . ||   || . .|| || . | . . . . .||",
+  "|| . .||   ||. _-|| ||-_ .||   ||. . || || . .||   ||. _-|| ||-_.|\\ . . . . ||",
+  "||. . ||   ||-'  || ||  `-||   || . .|| ||. . ||   ||-'  || ||  `|\\_ . .|. .||",
+  "|| . _||   ||    || ||    ||   ||_ . || || . _||   ||    || ||   |\\ `-_/| . ||",
+  "||_-' ||  .|/    || ||    \\|.  || `-_|| ||_-' ||  .|/    || ||   | \\  / |-_.||",
+  "||    ||_-'      || ||      `-_||    || ||    ||_-'      || ||   | \\  / |  `||",
+  "||    `'         || ||         `'    || ||    `'         || ||   | \\  / |   ||",
+  "||            .===' `===.         .==='.`===.         .===' /==. |  \\/  |   ||",
+  "||         .=='   \\_|-_ `===. .==='   _|_   `===. .===' _-|/   `==  \\/  |   ||",
+  "||      .=='    _-'    `-_  `='    _-'   `-_    `='  _-'   `-_  /|  \\/  |   ||",
+  "||   .=='    _-'          `-__\\._-'         `-_./__-'         `' |. /|  |   ||",
+  "||.=='    _-'                                                     `' |  /==.||",
+  "=='    _-'                        N E O V I M                         \\/   `==",
+  "\\   _-'                                                                `-_   /",
+  " `''                                                                      ``'  ",
+  "                                                                               ",
 }
 
-vim.g.dashboard_custom_footer = {'github.com/artofnothingness'}
+db.custom_center = {
+    {icon = '  ', desc = 'Recently files ',
+    action =  'Telescope oldfiles', shortcut = '  SPC f r'},
+    {icon = '  ', desc = 'Find  File     ',
+    action = 'Telescope find_files', shortcut = '  SPC f f'},
+    {icon = '  ', desc = 'Find  word     ',
+    action = 'Telescope live_grep', shortcut = '  SPC f w'},
+    {icon = '  ', desc = 'File Browser   ',
+    action = 'RnvimrToggle', shortcut = '  m r    '},
+    {icon = '  ', desc = 'New File       ',
+    action = 'DashboardNewFile', shortcut = '         '},
+    {icon = '  ', desc = 'Open config    ', 
+    action = ':e ~/.config/nvim/init.vim', shortcut = '         '},
+}
 
-vim.cmd("hi! dashboardHeader   guifg=#9788b9")
-vim.cmd("hi! dashboardCenter   guifg=#51afef")
-vim.cmd("hi! dashboardShortcut guifg=#9788b9")
-vim.cmd("hi! dashboardFooter   guifg=#586268")
+db.custom_footer = 'github.com/artofnothingness'
+
+-- vim.cmd("hi! dashboardHeader   guifg=#9788b9")
+-- vim.cmd("hi! dashboardCenter   guifg=#51afef")
+-- vim.cmd("hi! dashboardShortcut guifg=#9788b9")
+-- vim.cmd("hi! dashboardFooter   guifg=#586268")
