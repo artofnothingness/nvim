e82259f208f4b8885c20c273865bbac49fdf5182
Alex <aibudyakov@sberautotech.ru>
2022-09-15 12:18:37 +0300

    add help

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,9 +22,10 @@ function()
   use {
     "hrsh7th/nvim-cmp",
     requires = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'saadparwaiz1/cmp_luasnip',
       'L3MON4D3/LuaSnip',
     },
