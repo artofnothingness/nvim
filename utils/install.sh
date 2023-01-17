17fd88509da2a4b21fddc47b31b39b85d5377404
artofnothingness <budyakov.aleksei@gmail.com>
2023-01-17 20:52:11 +0300

    simplify configs

--- utils/install.sh
+++ utils/install.sh
@@ -2,16 +2,12 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo add-apt-repository -y ppa:git-core/ppa
 
 sudo apt update
-sudo apt install neovim-runtime=`sudo apt policy neovim-runtime | grep "   0.9" | sed -n 1p | awk -F ' ' '{print $1}'` 
-sudo apt install neovim=`sudo apt policy neovim | grep "   0.9" | sed -n 1p | awk -F ' ' '{print $1}'` -y
+sudo apt install \
+    neovim -y \
+    git -y \
+    python3-pip \
+    ripgrep \
+    xclip \
+    ccls -y
 
-sudo apt install python3-pip ripgrep xclip ccls -y
-pip install cmake-language-server
-pip install pynvim
-
-sudo apt-get install git -y
-
-sudo apt update
-
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+pip install cmake-language-server pynvim
