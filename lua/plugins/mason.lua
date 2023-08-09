635b332e5d752f6220b5f09af641e06944435882
Alex <aibudyakov@sberautotech.ru>
2023-08-09 14:03:20 +0300

    upd

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -8,6 +8,6 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function() require('plugins/cfg/mason-lspconfig') end,
         lazy = false,
-        dependencies = { 'neovim/nvim-lspconfig' },
+        dependencies = { 'neovim/nvim-lspconfig', 'williamboman/mason.nvim' },
     }
 }
