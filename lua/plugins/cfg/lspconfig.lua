2eca355106ce587db63705e6946ca44f514e8d89
Alex <aibudyakov@sberautotech.ru>
2023-09-18 15:36:00 +0300

    remove pyright

--- lua/plugins/cfg/lspconfig.lua
+++ lua/plugins/cfg/lspconfig.lua
@@ -21,15 +21,6 @@ require'lspconfig'.clangd.setup{
     root_dir = util.root_pattern(".git"),
  }
 
-require('lspconfig').pyright.setup {
-    on_attach = function(client, bufnr)
-        if lsp_status ~= nil then
-            lsp_status.on_attach(client, bufnr)
-        end
-    end,
-    plugins = { pylint = { enabled = true, executable = 'pylint', args = { '--rcfile', '~/.pylintrc' } } },
-}
-
 require('lspconfig').lua_ls.setup {
     settings = {
         Lua = {
