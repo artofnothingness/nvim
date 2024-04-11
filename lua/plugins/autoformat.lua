720c2bfae955b7a35359bcf8bb13ab1382ccbed2
Alex <aibudyakov@sberautotech.ru>
2024-04-11 16:29:26 +0300

    Add autoformatters

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -3,11 +3,19 @@ return { -- Autoformat
   opts = {
     notify_on_error = false,
     format_on_save = {
-      timeout_ms = 500,
+      timeout_ms = 5000,
       lsp_fallback = true,
     },
     formatters_by_ft = {
       lua = { 'stylua' },
+      cpp = { 'clang-format' },
+      python = function(bufnr)
+        if require('conform').get_formatter_info('ruff_format', bufnr).available then
+          return { 'ruff_format' }
+        else
+          return { 'isort', 'black' }
+        end
+      end,
     },
   },
 }
