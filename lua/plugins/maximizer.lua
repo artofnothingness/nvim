eecee9e6c4968a8ce7f1b58ac7cf7731107a5837
artofnothingness <speedy_sky@mail.ru>
2024-06-02 12:36:45 +0300

    add zen

--- lua/plugins/maximizer.lua
+++ lua/plugins/maximizer.lua
@@ -2,7 +2,7 @@ return {
   {
     'szw/vim-maximizer',
     config = function()
-      vim.keymap.set('n', '<leader>z', '<cmd>MaximizerToggle!<CR>', { desc = '[Z]oom buffer' })
+      vim.keymap.set('n', '<leader>Z', '<cmd>MaximizerToggle!<CR>', { desc = '[Z]oom buffer' })
     end,
   },
 }
