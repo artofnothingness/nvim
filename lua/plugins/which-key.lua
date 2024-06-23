c6e1dad9c8b285616b3ebaae00988c70f57fd11e
artofnothingness <speedy_sky@mail.ru>
2024-06-23 13:31:17 +0300

    update

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,7 +4,6 @@ return {
   config = function() -- This is the function that runs, AFTER loading
     require('which-key').setup()
 
-    -- Document existing key chains
     require('which-key').register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
       ['<leader>cd'] = { name = '[C]ode [D]iagnostics', _ = 'which_key_ignore' },
@@ -15,7 +14,5 @@ return {
       ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
       ['<leader>u'] = { name = '[U]ndo tree', _ = 'which_key_ignore' },
     }
-
-    require('which-key').register({ ['<leader>t'] = { name = '[T]abularize', _ = 'which_key_ignore' } }, { mode = 'v' })
   end,
 }
